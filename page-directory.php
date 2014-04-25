614b7ed68f2abef393c641d9c7c5bd6cfc56ce08
Jack Reid <emailjackreid@gmail.com>
2014-04-25 16:05:44 +0100

    Fix for page judder bug #1

--- page-directory.php
+++ page-directory.php
@@ -15,7 +15,7 @@ get_header(); ?>
 		<li class="button"><a href="http://pearshapedexeter.com/directory/promoters">Promoters</a></li>
 	</ul>
 </div>
-<div class="page-wrapper">
+<div class="page-wrapper directory-pad">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<?php the_content(); ?>
 	<?php endwhile; else: ?>
