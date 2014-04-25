83dba7b0d6fe53dc2b82cb352942ea528de63cf9
Jack Reid <emailjackreid@gmail.com>
2014-04-25 12:34:17 +0100

    prelimenary listings templates and styles

--- category-listings.php
+++ category-listings.php
@@ -8,4 +8,18 @@
 		<li class="button"><a href="http://pearshapedexeter.com/category/event-reviews">Reviews</a></li>
 	</ul>
 </div>
+
+<div class="listings-stream">
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<a href="<?php the_permalink(); ?>">
+		<h1 class="preview-title"><?php the_title(); ?></h1>
+		<div class="listings-content">
+			<?php the_content(); ?>
+		</div>
+	</a>
+	<?php endwhile; else: ?>
+	<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+	<?php endif; ?>
+</div>
+
 <?php get_footer(); ?>
