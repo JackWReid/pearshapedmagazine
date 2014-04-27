01247ef5bdeeb2a089a1b969b25ba7fc8cee025d
Jack Reid <emailjackreid@gmail.com>
2014-04-27 17:37:37 +0100

    fixed listings link

--- category-listings.php
+++ category-listings.php
@@ -3,7 +3,7 @@
 	<h1 class="section-title">Events</h1>
 
 	<ul class="category-toggler">
-		<li class="button-active"><a href="http://pearshapedexeter.com/category/listings-2">Listings</a></li>
+		<li class="button-active"><a href="http://pearshapedexeter.com/category/listings">Listings</a></li>
 		<li class="button"><a href="http://pearshapedexeter.com/category/event-previews">Upcoming</a></li>
 		<li class="button"><a href="http://pearshapedexeter.com/category/event-reviews">Reviews</a></li>
 	</ul>
