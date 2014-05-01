06884766592bc2ffed405f67da181449ef467ebb
Jack Reid <emailjackreid@gmail.com>
2014-05-01 15:15:43 +0100

    Reverse post order for cat box for event previews

--- home.php
+++ home.php
@@ -9,7 +9,7 @@
 
 	<div class="cat-box">
 		<?php $cat_id = 580;
-		$latest_cat_post = new WP_Query( array('posts_per_page' => 1, 'category__in' => array($cat_id)));
+		$latest_cat_post = new WP_Query( array('posts_per_page' => 1, 'category__in' => array($cat_id), 'order' => 'ASC'));
 		if( $latest_cat_post->have_posts() ) : while( $latest_cat_post->have_posts() ) : $latest_cat_post->the_post();  ?>
 		<div class="post-category-info">
 			<?php the_category (' &gt; '); ?>
