59ab597d7671e688f19d3381f52ec112e5f8425a
Jack Reid <emailjackreid@gmail.com>
2014-05-18 12:27:04 +0100

    header static for mobile fixes #24

--- js/sticky-top-bar.js
+++ js/sticky-top-bar.js
@@ -1,14 +1,20 @@
 (function($) {
 $(document).ready(function() {
 var stickyNavTop = $('.top-bar').offset().top;
-var stickyNav = function(){
-var scrollTop = $(window).scrollTop();
 
-if (scrollTop > stickyNavTop) {
-    $('.top-bar').addClass('sticky');
-} else {
-    $('.top-bar').removeClass('sticky');
-}
+var stickyNav = function(){
+
+	if($(window).width() > 650){
+		var scrollTop = $(window).scrollTop();
+		if (scrollTop > stickyNavTop) {
+		    $('.top-bar').addClass('sticky');
+		} else {
+		    $('.top-bar').removeClass('sticky');
+		}
+	} else {
+		$('.top-bar').removeClass('sticky');
+	}
+
 };
 
 stickyNav();
