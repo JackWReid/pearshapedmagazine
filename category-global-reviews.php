139afb6f40b0dcdb96f5107d02b6a98f13b29c40
jackwreid <emailjackreid@gmail.com>
2014-09-10 11:47:15 +0100

    Updated album reviews grid

--- category-global-reviews.php
+++ category-global-reviews.php
@@ -9,18 +9,17 @@
 	</ul>
 </div>
 
-<div class="thumbnail-grid">
 <?php
 $current_month = get_the_time('F');
 $current_year = get_the_time('Y');
-echo "<div class='date-divider'><div class='date-divider__info'><h3>" . $current_month . "</h3><h3>" . $current_year . "</h3></div></div>";
+echo "<div class='date-divider'><h3>" . $current_month. " " . $current_year . "</h3></div><div class='thumbnail-grid'>";
 
 while (have_posts()) : the_post();
     $current_year = get_the_time('Y');
     $this_month = get_the_time('F');
     if( $this_month!=$current_month ):
         $current_month = $this_month;
-        echo "<div class='date-divider'><div class='date-divider__info'><h3>" . $current_month . "</h3><h3>" . $current_year . "</h3></div></div>";
+        echo "</div><div class='date-divider'><h3>" . $current_month . " " . $current_year . "</h3></div><div class='thumbnail-grid'>";
     endif;
 
     $author = get_post_meta($post->ID, 'Author', true);
@@ -30,7 +29,7 @@ while (have_posts()) : the_post();
     if ($author) { echo "<p>" . $author . "</p></div>"; }
     echo "</a></div>";
 endwhile;
+echo "</div>";
 ?>
-</div>
 
 <?php get_footer(); ?>
