647bd5666b0c9ea915660c0bc8c42cbffc165697
jackwreid <emailjackreid@gmail.com>
2014-09-11 00:02:56 +0100

    Catch up repo with new gitignore

--- dist/PearShapedMagazine/404.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<div class="section-header">
-	<h1 class="section-header__title">Are you lost?</h1>
-</div>
-<div class="fourohfour">
-	<img src="<?php echo site_url(); ?>/wp-content/uploads/2014/05/Roundel-Logo.svg" alt="PearShaped Logo">
-	<h2>I'm afraid we couldn't find the stuff you were looking for. Maybe try a search?</h2>
-</div>
-<?php get_footer(); ?>
