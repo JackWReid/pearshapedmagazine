4c5f19f14c4a8a5bfc4e60de785f41069e0d6f24
JackWReid <emailjackreid@gmail.com>
2014-11-09 10:31:25 +0000

    removed contact page template (redundant

--- page.php
+++ page.php
@@ -9,12 +9,10 @@ get_header(); ?>
 <div class="section-header">
 	<h1 class="section-header__title"><?php the_title(); ?></h1>
 </div>
-<div class="page-wrapper">
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<?php the_content(); ?>
-	<?php endwhile; else: ?>
-	<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-	<?php endif; ?>
-</div>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php the_content(); ?>
+<?php endwhile; else: ?>
+<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
 
 <?php get_footer(); ?>
