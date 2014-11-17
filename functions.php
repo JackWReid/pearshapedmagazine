7e742905027c924393dbbdfaac05b3bfa4a35229
JackWReid <emailjackreid@gmail.com>
2014-11-17 10:46:56 +0000

    simplified functions.php

--- functions.php
+++ functions.php
@@ -6,9 +6,9 @@
 ?>
 <?php function add_my_script() {
 	wp_enqueue_script(
-	    'sticky-top-bar', // name your script so that you can attach other scripts and de-register, etc.
-	    get_template_directory_uri() . '/js/sticky-top-bar.js', // this is the location of your script file
-	    array('jquery') // this array lists the scripts upon which your script depends
+	    'sticky-top-bar',
+	    get_template_directory_uri() . '/js/sticky-top-bar.js',
+	    array('jquery')
 	);
 	}
 ?>
