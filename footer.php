5770b701e1dba9888e860a3cc28de1a09bb3475e
Jack Reid <emailjackreid@gmail.com>
2015-09-15 13:53:29 +0100

    Add temporary sign up prompt in site footer

--- footer.php
+++ footer.php
@@ -1,30 +1,32 @@
 <div class="footer">
-	<?php wp_footer(); ?>
-  <div class="footer-alt">
+  <div class="footer__signup">
+    <a href="https://www.exeterguild.org/societies/PearShaped/">Want to write for us? Join PearShaped here.</a>
+  </div>
+  <div class="footer__inner">
     <h2>PearShaped Magazine</h2>
     <small>est. 2013</small>
-    <div class="footer-nav">
-      <ul class="footer-list">
+    <div class="footer__inner__nav">
+      <ul class="footer__inner__nav__list">
         <a href="<?php echo site_url(); ?>/"><li>Home</li></a>
         <a href="<?php echo site_url(); ?>/contact-us/"><li>Contact Us</li></a>
         <a href="https://www.exeterguild.org/societies/15967/"><li>Join</li></a>
       </ul>
-      <ul class="footer-list">
+      <ul class="footer__inner__nav__list">
         <a href="<?php echo site_url(); ?>/category/events"><li>Events</li></a>
         <a href="<?php echo site_url(); ?>/category/event-previews/"><li>Upcoming</li></a>
         <a href="<?php echo site_url(); ?>/category/event-reviews/"><li>Reviews</li></a>
       </ul>
-      <ul class="footer-list">
+      <ul class="footer__inner__nav__list">
         <a href="<?php echo site_url(); ?>/category/reviews/"><li>Reviews</li></a>
         <a href="<?php echo site_url(); ?>/category/local-reviews/"><li>Local</li></a>
         <a href="<?php echo site_url(); ?>/category/global-reviews/"><li>Global</li></a>
       </ul>
-      <ul class="footer-list">
+      <ul class="footer__inner__nav__list">
         <a href="<?php echo site_url(); ?>/category/features/"><li>Features</li></a>
         <a href="<?php echo site_url(); ?>/category/interview/"><li>Interviews</li></a>
         <a href="<?php echo site_url(); ?>/category/guides/"><li>Guides</li></a>
       </ul>
-      <ul class="footer-list">
+      <ul class="footer__inner__nav__list">
         <a href="<?php echo site_url(); ?>/category/local-reviews/"><li>Columns</li></a>
         <a href="<?php echo site_url(); ?>/category/listening_post/"><li>Listening Post</li></a>
         <a href="<?php echo site_url(); ?>/category/spotlight/"><li>Spotlight</li></a>
@@ -33,6 +35,7 @@
     </div>
     <small>Website designed by Jack Reid &amp; Lizzie Hatfield. Developed by Jack Reid.</small>
   </div>
+  <?php wp_footer(); ?>
 </div>
 </script>
 </body>
